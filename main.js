627d048a0f7d5ad24889809bf0d7824028da66cb
Juanes <juanesc369369@gmail.com>
2024-05-10 22:06:48 -0500

    commit

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
-
+// Función para manejar la búsqueda
 function handleSearch() {
-    // Obtener el valor del input de búsqueda
     var query = document.getElementById('searchInput').value;
-    
-    // Redirigir al usuario a la página de resultados según el término de búsqueda
+    // Redirige al usuario a la página de resultados según el término de búsqueda
     window.location.href = "pages/tienda.html?q=" + encodeURIComponent(query);
 }
-document.addEventListener('DOMContentLoaded', function() {
-    showPasswordPrompt();
-});
 
-let loginShown = false;
-
-document.addEventListener('DOMContentLoaded', function() {
-    if (!loginShown) {
-        showPasswordPrompt();
-        loginShown = true;
-    }
-});
-
+// Función para mostrar el cuadro de diálogo de inicio de sesión
 async function showPasswordPrompt() {
     const { value: password } = await Swal.fire({
         title: "Enter your password",
@@ -35,23 +22,25 @@ async function showPasswordPrompt() {
         cancelButtonText: "Cancel",
         showLoaderOnConfirm: true,
         preConfirm: (password) => {
+            // Devuelve una promesa que se resuelve después de 1 segundo (simulando una verificación de contraseña)
             return new Promise((resolve) => {
                 setTimeout(() => {
-                    if (password === "1234") {
-                        resolve();
+                    if (password === "1234") { // Reemplaza "1234" con tu lógica de validación de contraseña
+                        resolve(); // Resuelve la promesa si la contraseña es correcta
                     } else {
-                        Swal.showValidationMessage("Invalid password!");
+                        Swal.showValidationMessage("Invalid password!"); // Muestra un mensaje de error si la contraseña es incorrecta
                     }
-                }, 1000);
+                }, 1000); // Espera 1 segundo antes de resolver la promesa
             });
         }
     });
 
     if (password) {
-        loginSuccess();
+        loginSuccess(); // Llama a la función loginSuccess() si la contraseña es válida
     }
 }
 
+// Función para manejar el inicio de sesión exitoso
 function loginSuccess() {
     Swal.fire({
         icon: "success",
@@ -62,7 +51,12 @@ function loginSuccess() {
     }).then(() => {
         // Redirige solo si la página de inicio no es la actual
         if (window.location.pathname !== "/index.html") {
-            window.location.href = "../index.html";
+            window.location.href = "../index.html"; // Redirige a la página de inicio después del inicio de sesión exitoso
         }
     });
 }
+
+// Event listener para mostrar el cuadro de diálogo de inicio de sesión cuando la página se carga
+document.addEventListener('DOMContentLoaded', function() {
+    showPasswordPrompt();
+});
